a3c0987fd31ec4e4e7a2c8c56ed35202f039df30
Tom Wang <zww@umich.edu>
2020-04-29 01:02:22 -0400

    fix merge.js eslint

--- merge.js
+++ merge.js
@@ -2,14 +2,12 @@ function mergeAll() {
   chrome.windows.getAll((windows) => {
     if (windows.length > 1) {
       chrome.tabs.query({ currentWindow: true, active: true }, (currentTab) => {
-        console.log(currentTab.length);
-        currentTab = currentTab[0];
         chrome.tabs.query({}, (allTabs) => {
           const list = [];
-          for (let i = 0; i < allTabs.length; ++i) {
-            if (allTabs[i].id != currentTab.id) list.push(allTabs[i].id);
+          for (let i = 0; i < allTabs.length; i += 1) {
+            if (allTabs[i].id !== currentTab[0].id) list.push(allTabs[i].id);
           }
-          chrome.tabs.move(list, { windowId: currentTab.windowId, index: -1 });
+          chrome.tabs.move(list, { windowId: currentTab[0].windowId, index: -1 });
         });
       });
       window.location.reload();
